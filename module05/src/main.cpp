840ebb1b0c9b7c4dd1161b954e2b5d15864217d0
Alabar <hugollmarta@hotmail.com>
2026-01-31 15:45:17 +0000

    add minimal main for unit tests

---
+++ module05/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Railway Simulation - Week 1 Development Build" << std::endl;
+	std::cout << "Node and Rail classes implemented. Full simulation coming soon." << std::endl;
+	return 0;
+}
